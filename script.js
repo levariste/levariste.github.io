0438ec500d3358b6d5e6b310be111d87041cc9f7
pauldubois98 <pauldubois98@gmail.com>
2024-08-02 14:14:22 +0200

    renamed var to button

--- script.js
+++ script.js
@@ -1,17 +1,15 @@
-const darkTheme = document.getElementById("dark-theme");
-const lightTheme = document.getElementById("light-theme");
-
-darkTheme.style.display = "none";
+const darkThemeButton = document.getElementById("dark-theme");
+const lightThemeButton = document.getElementById("light-theme");
 
 function toggleTheme() {
-  if (darkTheme.style.display === "none") {
-    darkTheme.style.display = "block";
-    lightTheme.style.display = "none";
+  if (darkThemeButton.style.display === "none") {
+    darkThemeButton.style.display = "block";
+    lightThemeButton.style.display = "none";
   } else {
-    darkTheme.style.display = "none";
-    lightTheme.style.display = "block";
+    darkThemeButton.style.display = "none";
+    lightThemeButton.style.display = "block";
   }
   document.body.classList.toggle("dark-mode");
 }
-darkTheme.addEventListener("click", toggleTheme);
-lightTheme.addEventListener("click", toggleTheme);
+darkThemeButton.addEventListener("click", toggleTheme);
+lightThemeButton.addEventListener("click", toggleTheme);
