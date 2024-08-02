89c014472cc0aa93c5a68ec0e6c7c6c13f317e96
pauldubois98 <pauldubois98@gmail.com>
2024-08-02 14:19:51 +0200

    dropped toggle theme

--- script.js
+++ script.js
@@ -1,16 +1,6 @@
 const darkThemeButton = document.getElementById("dark-theme");
 const lightThemeButton = document.getElementById("light-theme");
 
-function toggleTheme() {
-  if (darkThemeButton.style.display === "none") {
-    darkThemeButton.style.display = "block";
-    lightThemeButton.style.display = "none";
-  } else {
-    darkThemeButton.style.display = "none";
-    lightThemeButton.style.display = "block";
-  }
-  document.body.classList.toggle("dark-mode");
-}
 function lightTheme() {
   document.body.classList.remove("dark-mode");
   darkThemeButton.style.display = "block";
