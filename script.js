c30619390c22c96c10b0a516c14abab4cbf10ab8
pauldubois98 <pauldubois98@gmail.com>
2024-08-02 14:15:01 +0200

    store theme in local storage

--- script.js
+++ script.js
@@ -15,11 +15,21 @@ function lightTheme() {
   document.body.classList.remove("dark-mode");
   darkThemeButton.style.display = "block";
   lightThemeButton.style.display = "none";
+  localStorage.setItem("theme", "light");
 }
 function darkTheme() {
   document.body.classList.add("dark-mode");
   darkThemeButton.style.display = "none";
   lightThemeButton.style.display = "block";
+  localStorage.setItem("theme", "dark");
 }
 darkThemeButton.addEventListener("click", darkTheme);
 lightThemeButton.addEventListener("click", lightTheme);
+
+// get theme from local storage
+const localTheme = localStorage.getItem("theme");
+if (localTheme === "dark") {
+  darkTheme();
+} else {
+  lightTheme();
+}
